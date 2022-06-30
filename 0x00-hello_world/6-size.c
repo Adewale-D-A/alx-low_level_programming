5f4481063a7e69114768aa9edd3065ef4f6845db
adewale-d-a <adewale.d.a@outlook.com>
2022-06-30 22:31:03 +0100

    sizes of different datatypes 5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-char str = "Hello";
+char str = 'Hello';
 int num = 1;
 long int num2 = 255568;
 long long int num3 = 58697;
 float flo = 2.55;
 printf("Size of a char: %c byte(s)\n", sizeof(str));
 printf("Size of an int: %d byte(s)\n", sizeof(num));
-printf("Size of a long int: %ld byte(s)\n", sizeof(numb2));
+printf("Size of a long int: %ld byte(s)\n", sizeof(num2));
 printf("Size of a long long int: %lld byte(s)\n", sizeof(num3));
 printf("Size of a float: %f byte(s)\n", sizeof(flo));
 return (0);
