ea433048a4d8932405f6de5d569e99f9cb5406fb
adewale-d-a <adewale.d.a@outlook.com>
2022-06-30 22:12:17 +0100

    sizes of different datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str:
+int num:
+long int num2:
+long long int num3:
+float flo:
+printf("Size of a char: %zu byte(s)\n", sizeof(str))
+printf("Size of a int: %zu byte(s)\n", sizeof(num))
+printf("Size of a long int: %zu byte(s)\n", sizeof(num2))
+printf("Size of long long int: %zu byte(s)\n", sizeof(num3))
+printf("Size of float: %zu byte(s)\n", sizeof(flo))
+return (0);
+}
