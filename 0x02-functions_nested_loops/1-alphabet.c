f5fb1b4bff00738868dc1c0aaffa1eb3efe4e032
adewale-d-a <adewale.d.a@outlook.com>
2022-07-06 20:18:03 +0100

    alphabet 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
-#include "print_alphabet_x10.h"
+#include "main.h"
 
 /**
  * print_alphabet - Check description
  * Description: It prints the alphabet in lowercase fallowed by a new line
  * Return: Always 0.
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 	char i;
 
