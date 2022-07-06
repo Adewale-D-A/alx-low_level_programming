571c8ed66d18dd4111cd502f829d50a6a9fd8fc2
adewale-d-a <adewale.d.a@outlook.com>
2022-07-06 22:14:24 +0100

    debugging 5

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,2 +1,4 @@
-main.c;
-int largest_number(int a, int b, int c);
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
