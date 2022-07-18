71512d88d881d92a51f3792a4cb2a940a9c8ed8f
adewale-d-a <adewale.d.a@outlook.com>
2022-07-18 20:21:12 +0100

    pointer to array 2.5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,20 +8,34 @@
  * Return: 0 if strings are the same
  * any other number if they are different.
  */
-
+int _strncmp(char *s1, char *s2, unsigned int b)
+{
+unsigned int i;
+for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
+{s1++;
+s2++;
+}
+return (i - b);
+}
+/**
+ * _strstr - locates a substring
+ * @haystack: string to look though
+ * @needle: substring to find
+ * Return: pointer to string with result of search
+ */
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int len;
-	/*Get length of needle for strncmp*/
-	len = 0;
-	while (needle[len] != '\0')
-		len++;
-	/*compare substring*/
-	while (*haystack != '\0')
-	{
-		if (_strncmp(haystack, needle, len) == 0)
-			return (haystack);
-		haystack++;
-	}
-	return (NULL);
+/*Get length of needle for strncmp*/
+len = 0;
+while (needle[len] != '\0')
+len++;
+/*compare substring*/
+while (*haystack != '\0')
+{
+if (_strncmp(haystack, needle, len) == 0)
+return (haystack);
+haystack++;
+}
+return (NULL);
 }
